f21e6dc94b97941669627de7f78ef213ce55e40a
inboxDallas <inboxDallas@gmail.com>
2024-04-29 16:12:05 -0700

    chore: updating the commit prompts

--- src/scripts/git-commit.mjs
+++ src/scripts/git-commit.mjs
@@ -1,54 +1,72 @@
-import { exec } from 'child_process';
+import { exec } from "child_process";
 
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
 
 const commitTypes = [
-  'build',
-  'chore',
-  'ci',
-  'docs',
-  'feat',
-  'fix',
-  'perf',
-  'refactor',
-  'revert',
-  'style',
-  'test',
+  {
+    name: "build: Changes that affect the build system or external dependencies",
+    value: "build",
+  },
+  {
+    name: "chore: Other changes that don't modify src or test files",
+    value: "chore",
+  },
+  {
+    name: "ci: Changes to our CI configuration files and scripts",
+    value: "ci",
+  },
+  { name: "docs: Documentation only changes", value: "docs" },
+  { name: "feat: A new feature", value: "feat" },
+  { name: "fix: A bug fix", value: "fix" },
+  { name: "perf: A code change that improves performance", value: "perf" },
+  {
+    name: "refactor: A code change that neither fixes a bug nor adds a feature",
+    value: "refactor",
+  },
+  { name: "revert: Reverts a previous commit", value: "revert" },
+  {
+    name: "style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
+    value: "style",
+  },
+  {
+    name: "test: Adding missing tests or correcting existing tests",
+    value: "test",
+  },
 ];
 
 inquirer
   .prompt([
     {
-      type: 'list',
-      name: 'commitAction',
-      message: 'Choose the commit action:',
-      choices: ['Standard Commit', 'Amend Last Commit'],
+      type: "list",
+      name: "commitAction",
+      message: "Choose the commit action:",
+      choices: ["Standard Commit", "Amend Last Commit"],
     },
     {
-      type: 'list',
-      name: 'type',
-      message: 'What type of git commit is this?',
+      type: "list",
+      name: "type",
+      message: "What type of git commit is this?",
       choices: commitTypes,
-      when: (answers) => answers.commitAction === 'Standard Commit',
+      when: (answers) => answers.commitAction === "Standard Commit",
     },
     {
-      type: 'input',
-      name: 'message',
-      message: 'Enter the commit message:',
+      type: "input",
+      name: "message",
+      message: "Enter the commit message:",
       validate: function (input) {
         if (input.length < 3) {
-          return 'Commit message must be longer than 3 characters.';
+          return "Commit message must be longer than 3 characters.";
         }
         return true;
       },
-      when: (answers) => answers.commitAction === 'Standard Commit',
+      when: (answers) => answers.commitAction === "Standard Commit",
     },
   ])
   .then((answers) => {
     let commitCommand;
 
-    if (answers.commitAction === 'Amend Last Commit') {
-      commitCommand = 'git commit --amend --no-edit';
+    if (answers.commitAction === "Amend Last Commit") {
+      commitCommand = "git commit --amend --no-edit";
     } else {
       commitCommand = `git commit -m "${answers.type}: ${answers.message}"`;
     }
