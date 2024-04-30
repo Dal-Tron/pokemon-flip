460bacea215a7b1c6b64cffd0fe714329e886bd8
inboxDallas <inboxDallas@gmail.com>
2024-04-30 16:19:17 -0700

    chore: removing emotion and minor style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#014177",
+      },
       fontFamily: {
         inherit: "inherit",
         headings: [
