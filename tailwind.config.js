dd9fcd9169d8cc475d4d461d9b8d1f4c49a06155
inboxDallas <inboxDallas@gmail.com>
2024-05-02 16:14:50 -0700

    feat: add shake and hold animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
       colors: {
         primary: "#014177",
       },
+      keyframes: {
+        "shake-and-pause": {
+          "0%, 100%": { transform: "rotate(0deg)" },
+          "10%, 30%, 50%, 70%, 90%": { transform: "rotate(-3deg)" },
+          "20%, 40%, 60%, 80%": { transform: "rotate(3deg)" },
+          "95%": { transform: "rotate(0deg)" },
+        },
+      },
+      animation: {
+        "shake-and-pause": "shake-and-pause 1.4s ease-in-out 2",
+      },
       fontFamily: {
         inherit: "inherit",
         headings: [
