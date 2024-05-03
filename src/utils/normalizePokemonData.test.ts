8546d7ef62c8ba51fec3de9af39cfbb93570ecb1
inboxDallas <inboxDallas@gmail.com>
2024-05-02 19:37:55 -0700

    feat: adding a new util for normalizing pokemon data

---
+++ src/utils/normalizePokemonData.test.ts
@@ -0,0 +1,56 @@
+import { normalizePokemonData } from "./normalizePokemonData";
+
+describe("normalizePokemonData", () => {
+  it("transforms API data into IPokemon format correctly", () => {
+    const mockApiData = {
+      name: "pikachu",
+      sprites: {
+        front_default: "http://example.com/pikachu.png",
+      },
+      stats: [
+        { stat: { name: "hp" }, base_stat: 35 },
+        { stat: { name: "attack" }, base_stat: 55 },
+        { stat: { name: "defense" }, base_stat: 40 },
+        { stat: { name: "speed" }, base_stat: 90 },
+      ],
+      types: [{ type: { name: "Electric" } }],
+    };
+
+    const expectedPokemon = {
+      name: "pikachu",
+      image: "http://example.com/pikachu.png",
+      hp: 35,
+      attack: 55,
+      defense: 40,
+      speed: 90,
+      types: ["electric"],
+    };
+
+    const result = normalizePokemonData(mockApiData);
+    expect(result).toEqual(expectedPokemon);
+  });
+
+  it("handles missing fields appropriately", () => {
+    const mockApiData = {
+      name: "unknown",
+      sprites: {
+        front_default: "",
+      },
+      stats: [],
+      types: [],
+    };
+
+    const expectedPokemon = {
+      name: "unknown",
+      image: "",
+      hp: 0,
+      attack: 0,
+      defense: 0,
+      speed: 0,
+      types: [],
+    };
+
+    const result = normalizePokemonData(mockApiData);
+    expect(result).toEqual(expectedPokemon);
+  });
+});
---
+++ src/utils/normalizePokemonData.ts
@@ -0,0 +1,26 @@
+import { ApiPokemonData, IPokemon } from "@/types/pokemon";
+
+export const normalizePokemonData = (data: ApiPokemonData): IPokemon => {
+  const stats: { [key: string]: number } = data.stats.reduce<{
+    [key: string]: number;
+  }>((acc, stat) => {
+    const key = stat.stat.name;
+    acc[key] = stat.base_stat;
+    return acc;
+  }, {});
+
+  const types = data.types?.map((typeObj) => {
+    const typeName = typeObj.type?.name?.toLowerCase();
+    return typeName;
+  });
+
+  return {
+    name: data.name || "",
+    image: data.sprites?.front_default || "",
+    hp: stats.hp || 0,
+    attack: stats.attack || 0,
+    defense: stats.defense || 0,
+    speed: stats.speed || 0,
+    types: types,
+  };
+};
